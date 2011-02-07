7b2d648f9b9a7d0728de6601d7cee9c0fbbb078d
Cohuman Developer <cohumanadmin@basil-2.local>
2011-02-07 15:11:15 -0800

    brand new rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Registrar::Application.config.secret_token = '73448bbff457078a4dcd70cac4c9d61fd8a71ac5f1c2434a059e1aee0c709c293d18c1e2885a0fda9fdc23a94052d6e36b05fcfc7e49e53ea693d4f64ab47322'
